92674ded9494bcc3e80471d49893105a9a4c2662
Griffin Young <griffinyoung50@gmail.com>
2020-11-15 15:50:11 -0800

    initial boilerplate

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <button @click="inc">+</button>
+    <button @click="dec">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    inc() {
+      this.count++;
+    },
+    dec() {
+      this.count--;
+    }
+  }
+};
+</script>
